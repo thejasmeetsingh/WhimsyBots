ad143b58b0931e0fd4de5a7d9d02effee89d9121
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2026-04-03 01:25:35 +0000

    Updated django header info

--- src/whimsybots/urls.py
+++ src/whimsybots/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.AdminSite.site_header = "WhimsyBots"
+admin.AdminSite.site_title = "WhimsyBots"
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("martor/", include("martor.urls"))
