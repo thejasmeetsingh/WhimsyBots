d835035933d5979bafc00139c781cee971dec521
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2026-04-09 13:44:15 +0000

    Added Ollama client module

---
+++ src/app/ollama.py
@@ -0,0 +1,49 @@
+import ollama
+
+
+class OllamaClient:
+    _client = None
+    _model = None
+
+    def __init__(self, endpoint=None, model=None):
+        _endpoint = self._get_clean_endpoint(endpoint)
+        self._client = ollama.Client(host=_endpoint)
+        self._model = model
+
+    @staticmethod
+    def _get_clean_endpoint(endpoint: str):
+        endpoint = endpoint.strip("/").replace("localhost", "host.docker.internal")
+        return endpoint
+
+    def update_endpoint(self, endpoint: str):
+        _endpoint = self._get_clean_endpoint(endpoint)
+        self._client = ollama.Client(host=_endpoint)
+
+    def update_model(self, model: str):
+        self._model = model
+
+    def list_models(self):
+        response = self._client.list()
+        models = list(map(lambda x: {"name": x.name, "model": x.model}, response.models))
+        return models
+
+    def chat(self, messages, tools, format, options):
+        response = self._client.chat(
+            model=self._model,
+            messages=messages,
+            tools=tools,
+            format=format,
+            options=options
+        )
+        message = response.message
+        tools = None
+
+        if message.tool_calls:
+            tools = list(map(lambda x: x["function"], message.tool_calls))
+
+        result = {
+            "message": message.content,
+            "tools": tools
+        }
+
+        return result
