7ce43fd8edda0c3b4bfc84fdbf308eebb6aebca1
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2026-04-09 15:21:36 +0000

    Added AppForm and updated OllamaForm for fetching ollama models dynamically

--- src/app/forms.py
+++ src/app/forms.py
@@ -3,15 +3,19 @@ import traceback
 
 from django import forms
 
-from app.models import Ollama
+from app.models import Ollama, App
 from app.ollama import OllamaClient
 
 
 logger = logging.getLogger(__name__)
 
 
+def get_model_field():
+    return forms.ChoiceField(choices=[(None, "Select a model")], required=False, help_text="Please select model which supports tool calling")
+
+
 class OllamaForm(forms.ModelForm):
-    default_model = forms.ChoiceField(choices=[], required=False)
+    default_model = get_model_field()
 
     class Meta:
         model = Ollama
@@ -22,13 +26,45 @@ class OllamaForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         try:
-            o_client = OllamaClient(endpoint=instance.endpoint if instance and instance.endpoint else None)
+            endpoint = instance.endpoint if instance and instance.endpoint else None
+            api_key = instance.api_key if instance and instance.api_key else None
+            o_client = OllamaClient(endpoint=endpoint, api_key=api_key)
+
             models = o_client.list_models()
-            choices = list(map(lambda x: (x["name"], x["name"], models)))
+            choices = list(map(lambda model: (model, model), models))
             self.fields["default_model"].choices = choices
         except Exception as e:
             logger.error({
-                "msg": "Error while fetching models from Ollama",
+                "msg": "OllamaForm | Error while fetching models from Ollama",
                 "error": str(e),
                 "traceback": traceback.format_exc()
             })
+
+
+class AppForm(forms.ModelForm):
+    ollama_model = get_model_field()
+
+    class Meta:
+        model = App
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        ollama_obj = Ollama.objects.first()
+        if ollama_obj:
+            try:
+                o_client = OllamaClient(endpoint=ollama_obj.endpoint, api_key=ollama_obj.api_key)
+                models = o_client.list_models()
+                choices = list(map(lambda model: (model, model), models))
+                
+                if ollama_obj.default_model:
+                    choices.sort(key=lambda x: x[0] != ollama_obj.default_model)
+
+                self.fields["ollama_model"].choices = choices
+            except Exception as e:
+                logger.error({
+                    "msg": "AppForm | Error while fetching models from Ollama",
+                    "error": str(e),
+                    "traceback": traceback.format_exc()
+                })
