e6c7e40339b7b9d82f64edb2ec62d3006d2adc68
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2026-04-09 13:56:30 +0000

    Updated ollama client module

--- src/app/ollama.py
+++ src/app/ollama.py
@@ -3,12 +3,10 @@ import ollama
 
 class OllamaClient:
     _client = None
-    _model = None
 
-    def __init__(self, endpoint=None, model=None):
-        _endpoint = self._get_clean_endpoint(endpoint)
+    def __init__(self, endpoint=None):
+        _endpoint = self._get_clean_endpoint(endpoint or "http://localhost:11434")
         self._client = ollama.Client(host=_endpoint)
-        self._model = model
 
     @staticmethod
     def _get_clean_endpoint(endpoint: str):
@@ -20,9 +18,9 @@ class OllamaClient:
         models = list(map(lambda x: {"name": x.name, "model": x.model}, response.models))
         return models
 
-    def chat(self, messages, tools, format, options):
+    def chat(self, model, messages, tools=None, format=None, options=None):
         response = self._client.chat(
-            model=self._model,
+            model=model,
             messages=messages,
             tools=tools,
             format=format,
