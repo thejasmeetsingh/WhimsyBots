9ff10969a207ee6d46fc8db49967b8319648b483
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2026-04-09 14:32:25 +0000

    Updated Ollama admin form

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,11 +1,17 @@
-import requests
+import logging
+import traceback
+
 from django import forms
 
 from app.models import Ollama
+from app.ollama import OllamaClient
+
+
+logger = logging.getLogger(__name__)
 
 
 class OllamaForm(forms.ModelForm):
-    default_model = forms.ChoiceField(choices=[], required=True)
+    default_model = forms.ChoiceField(choices=[], required=False)
 
     class Meta:
         model = Ollama
@@ -14,16 +20,15 @@ class OllamaForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         instance = kwargs.get("instance")
         super().__init__(*args, **kwargs)
-        
-        # TODO: Create a Ollama Client to fetch the available models from the Ollama server.
-        endpoint = instance.endpoint if instance.endpoint else "http://localhost:11434"
-        if endpoint[-1] == "/":
-            endpoint = endpoint[:-1]
-        
-        endpoint = endpoint.replace("localhost", "host.docker.internal")
-        
-        response = requests.get(url=f"{endpoint}/api/tags")
-        if response.status_code == 200:
-            models = response.json().get("models", [])
+
+        try:
+            o_client = OllamaClient(endpoint=instance.endpoint if instance and instance.endpoint else None)
+            models = o_client.list_models()
             choices = list(map(lambda x: (x["name"], x["name"], models)))
             self.fields["default_model"].choices = choices
+        except Exception as e:
+            logger.error({
+                "msg": "Error while fetching models from Ollama",
+                "error": str(e),
+                "traceback": traceback.format_exc()
+            })
