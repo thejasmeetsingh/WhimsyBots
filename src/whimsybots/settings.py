412d6a3098088446a45e9b4a0f568dc8288cf1c2
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2026-05-05 07:57:03 +0530

    refactor: Update settings for DB access and logging consistency
    - Extract DB configuration variables (DB_NAME, DB_USER, etc.) for reuse in other modules
    - Update DATABASES configuration to use these variables
    - Improve logging configuration formatting for better readability
    - Clean up INSTALLED_APPS formatting

--- src/whimsybots/settings.py
+++ src/whimsybots/settings.py
@@ -18,9 +18,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "app",
-
     "martor",
-
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -62,14 +60,19 @@ WSGI_APPLICATION = "whimsybots.wsgi.application"
 
 # Database
 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": 5432
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": 5432,
     }
 }
 
@@ -110,7 +113,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "whimsybots/static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL = "/admin/login/"
 
@@ -129,9 +132,17 @@ MARTOR_ENABLE_CONFIGS = {
 }
 
 MARTOR_TOOLBAR_BUTTONS = [
-    "bold", "italic", "horizontal", "heading", "pre-code",
-    "blockquote", "unordered-list", "ordered-list",
-    "emoji", "toggle-maximize", "help"
+    "bold",
+    "italic",
+    "horizontal",
+    "heading",
+    "pre-code",
+    "blockquote",
+    "unordered-list",
+    "ordered-list",
+    "emoji",
+    "toggle-maximize",
+    "help",
 ]
 
 CSRF_COOKIE_HTTPONLY = False
@@ -140,36 +151,38 @@ CSRF_COOKIE_HTTPONLY = False
 # Logging Config
 
 LOGGING_CONFIG = None
-logging.config.dictConfig({
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "()": "pythonjsonlogger.jsonlogger.JsonFormatter",
-            "format": "%(asctime)s - %(module)s.%(funcName)s - %(name)s - %(levelname)s - %(message)s - %(exc_text)s"
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "()": "pythonjsonlogger.jsonlogger.JsonFormatter",
+                "format": "%(asctime)s - %(module)s.%(funcName)s - %(name)s - %(levelname)s - %(message)s - %(exc_text)s",
+            },
+            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
         },
-        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "default",
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+            },
+            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
         },
-        "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-    },
-    "loggers": {
-        "": {
-            "level": "INFO",
-            "handlers": ["console"],
+        "loggers": {
+            "": {
+                "level": "INFO",
+                "handlers": ["console"],
+            },
+            "django.db.backends": {
+                "level": "INFO",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
         },
-        "django.db.backends": {
-            "level": "INFO",
-            "handlers": ["console"],
-            "propagate": False
-        },
-        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
-    },
-})
+    }
+)
 
 
 # Celery
